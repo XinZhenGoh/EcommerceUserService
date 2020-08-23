4d724f62df5954189577adcdf4e0c6dceb80fa39
Xin Zhen (Ray) Goh <xinzhengoh@gmail.com>
2020-08-23 14:45:52 -0700

    -Converting into Eureka Client

--- src/main/java/com/usermanagement/Application.java
+++ src/main/java/com/usermanagement/Application.java
@@ -3,11 +3,13 @@ package com.usermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.usermanagement.dao")
 @EntityScan(basePackages = "com.usermanagement.dto")
